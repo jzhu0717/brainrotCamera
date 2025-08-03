90792f21db016a87f595b734e39bf316d2ebc6ed
Joshua Zhu <joshuazjzhu@gmail.com>
2025-08-03 17:27:30 -0400

    video window

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # pip install (package name) 
 import cv2
 import boto3
-
+import time
 import credentials
 
 # create AWS rekognition client
@@ -25,21 +25,50 @@ if not video_capture.isOpened():
     print("Error: Could not open video source.")
     exit()
 
+last_check = 0
+check_interval = 2  # seconds between Rekognition calls to save API costs
+
 ret = True
 while ret:
     ret, frame = video_capture.read()
 
-    # convert to jpg
-    _, buffer = cv2.imencode('.jpg', frame) 
 
-    # convert buffer to bytes
-    image_bytes = buffer.tobytes()
-    break
+    if not ret:
+        print("Error: Could not read frame.")
+        break
 
-    # detect object
-    res = reko_client.detect_labels(
-        Image={'Bytes': image_bytes},
-        MinConfidence=50)
+    # open window
+    cv2.imshow('Live Webcam Feed', frame)
     
+    # Run every n seconds
+    if time.time() - last_check >= check_interval:
+        _, buffer = cv2.imencode('.jpg', frame)
+        image_bytes = buffer.tobytes()
 
+        res = reko_client.detect_labels(
+            Image={'Bytes': image_bytes},
+            MinConfidence=95) # this is the confidence threshold
+
+        print("Objects detected:", [label['Name'] for label in res['Labels']])
+        last_check = time.time()
+
+    # exit if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+    # # convert to jpg
+    # _, buffer = cv2.imencode('.jpg', frame) 
+
+    # # convert buffer to bytes
+    # image_bytes = buffer.tobytes()
+    # break
+
+    # # detect object
+    # res = reko_client.detect_labels(
+    #     Image={'Bytes': image_bytes},
+    #     MinConfidence=50)
+    
+video_capture.release()
+cv2.destroyAllWindows()
 # write detections
